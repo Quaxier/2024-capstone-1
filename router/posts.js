285973232ff1ec6461c10ef852be6bfe2398c0ec
Quaxier <mjaudwo1@gmail.com>
2024-04-17 23:41:59 +0900

    post 에러 수정2

--- router/posts.js
+++ router/posts.js
@@ -4,19 +4,18 @@ const Post = require('../schema/post');
 
 const router = express.Router();
 
-router.route('/').post(async (req, res, next)=>{ // 게시글 작성 기능
+router.post('/', async(req, res, next)=>{ // 게시글 작성 기능
     try{
         console.log('게시글 작성 요청 수신됨!');
         console.log(req.body);
-        const post = await Post.create({
-            user_id: req.body.user_id,
+        await Post.create({
+            user_id: "661caf8ea5ee20100b82f81f",
             post_title: req.body.title,
-            post_date: req.body.date,
-            post_view: req.body.view,
-            post_content: req.body.content,
+            post_view: "0",
+            post_content: req.body.content
         });
-        console.log('새 게시글 작성됨: \n' + title);
-        res.status(201).json(user); // 응답시 HTML 상태코드를 지정하고 json형식의 응답을 보낸다
+        console.log('새 게시글 작성됨: \n');
+        res.status(201).json('성공!'); // 응답시 HTML 상태코드를 지정하고 json형식의 응답을 보낸다
     } catch(err) {
         console.error(err);
         next(err);
